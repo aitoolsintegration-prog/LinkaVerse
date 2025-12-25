895972f0f1cc438bbf5b6db93c0707106ff54c61
aitoolsintegration <aitoolsintegration@gmail.com>
2025-12-25 15:04:28 +1100

    Initial Commit

---
+++ app/src/main/java/com/example/linkaverse/MainActivity.java
@@ -0,0 +1,176 @@
+package com.thematicbiblestudy.linkaverse;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.content.Intent;
+import android.webkit.JavascriptInterface;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+   // String websiteURL = "https://thematic-bible-study-1069205335378.us-west1.run.app/"; // sets web url
+   // private WebView webview;
+   WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if( ! CheckNetwork.isInternetAvailable(this)) //returns true if internet available
+        {
+            //if there is no internet do this
+            setContentView(R.layout.activity_main);
+            //Toast.makeText(this,"No Internet Connection, Chris",Toast.LENGTH_LONG).show();
+
+            new AlertDialog.Builder(this) //alert the person knowing they are about to close
+                    .setTitle("No internet connection available")
+                    .setMessage("Please Check you're Mobile data or Wifi network.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    //.setNegativeButton("No", null)
+                    .show();
+
+        }
+        else
+        {
+         /*   webview = findViewById(R.id.webView);
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+
+            // Add the JavaScript interface here:
+            webview.addJavascriptInterface(
+                    new WebAppInterface(this),
+                    "Android"
+            );
+
+            webview.loadUrl(websiteURL);
+            webview.setWebViewClient(new WebViewClientDemo()); */
+
+           // super.onCreate(savedInstanceState);
+           // setContentView(R.layout.activity_main);
+
+            webView = findViewById(R.id.webView);
+
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+
+            webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("https://thematic-bible-study-1069205335378.us-west1.run.app/");
+        }
+
+    }
+
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        //Keep webview in app when clicking links
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    //set back button functionality
+  /*  @Override
+    public void onBackPressed() { //if user presses the back button do this
+        if (webview.isFocused() && webview.canGoBack()) { //check if in webview and the user can go back
+            webview.goBack(); //go back in webview
+        } else { //do this if the webview cannot go back any further
+
+            new AlertDialog.Builder(this) //alert the person knowing they are about to close
+                    .setTitle("EXIT")
+                    .setMessage("Are you sure. You want to close this app?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
+    }*/
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+}
+
+class CheckNetwork {
+
+    private static final String TAG = CheckNetwork.class.getSimpleName();
+
+    public static boolean isInternetAvailable(Context context)
+    {
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null)
+        {
+            Log.d(TAG,"no internet connection");
+            return false;
+        }
+        else
+        {
+            if(info.isConnected())
+            {
+                Log.d(TAG," internet connection available...");
+                return true;
+            }
+            else
+            {
+                Log.d(TAG," internet connection");
+                return true;
+            }
+
+        }
+    }
+}
+
+
+
+
+class WebAppInterface {
+    private Context context;
+
+    WebAppInterface(Context context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void shareBibleText(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(
+                Intent.createChooser(intent, "Share via")
+        );
+    }
+}
+
+
